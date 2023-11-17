62647d3c569db44fd6a5c831209ef81693d0303b
DavixVulcan <78835855+DavixVulcan@users.noreply.github.com>
2023-11-17 13:10:25 -0600

    Added wrapper, updated descriptor and title in wrapper

--- main.js
+++ main.js
@@ -32,30 +32,16 @@ function camera_composition(pos_x, pos_y, pos_z, rot_x, rot_y, rot_z){
 
 // My map of locations and rotations for the camera
 const positions = new Map([
-	["Start", new camera_composition(1,1,5,0,0,0)]
+	["Start", new camera_composition(1,1,5,0,0,0)],
+	["HackUTA 2023", new camera_composition(1,5,8,0,0,0)]
 ])
 
 // Map of descriptions
 const descriptions = new Map ([
-	["Start", "Hello, welcome to the space. Look around, take a gander at what I've worked on. "]
+	["Start", "Hello, welcome to the space. Look around, take a gander at what I've worked on. "],
+	["HackUTA 2023", "A digital AI photobooth"]
 ])
 
-// This function takes in a camera_allocator object, that extracts
-// the position and rotation, and tweens to it
-function move_to(p){
-	let coords_rots = { 
-		x: camera.position.x, y: camera.position.y, z: camera.position.z, 
-		xr: camera.rotation.x, yr: camera.rotation.y, zr: camera.rotation.z
-	};
-	new TWEEN.Tween(coords_rots).to(
-		{x: p.x, y: p.y, z: p.z, xr: p.xr, yr: p.yr, zr: p.zr}
-	).onUpdate(() => {
-		camera.position.set(coords_rots.x, coords_rots.y, coords_rots.z);
-		camera.rotation.set(coords_rots.xr, coords_rots.yr, coords_rots.zr);
-		// console.log("HAH");
-	}
-	).easing(TWEEN.Easing.Cubic.InOut).start();
-}	
 
 // Toggle drop down
 // let dropTog = false;
@@ -73,7 +59,7 @@ function drop_down_toggle(){
 		console.log("Opening");
 	}
 }
-
+// Bring it as an onclick function
 window.drop_down_toggle = drop_down_toggle;
 
 // Animation loop
@@ -107,7 +93,36 @@ function update_descriptor(flavorText){
 	}, 200);
 }
 
+// This function takes in a camera_allocator object, that extracts
+// the position and rotation, and tweens to it
+function move_to(p){
+	let coords_rots = { 
+		x: camera.position.x, y: camera.position.y, z: camera.position.z, 
+		xr: camera.rotation.x, yr: camera.rotation.y, zr: camera.rotation.z
+	};
+	new TWEEN.Tween(coords_rots).to(
+		{x: p.x, y: p.y, z: p.z, xr: p.xr, yr: p.yr, zr: p.zr}
+	).onUpdate(() => {
+		camera.position.set(coords_rots.x, coords_rots.y, coords_rots.z);
+		camera.rotation.set(coords_rots.xr, coords_rots.yr, coords_rots.zr);
+		// console.log("HAH");
+	}
+	).easing(TWEEN.Easing.Cubic.InOut).start();
+}
 
-move_to(positions.get("Start"));
+// A wrapper for our onclick function
+function selector(text){
+	const Title = document.getElementById("Title");
+	let new_pos = positions.get(text);
+	let new_text = descriptions.get(text);
+	Title.innerHTML = text;
+	Title.style.animation = 'none'; 
+	Title.offsetHeight;
+	Title.style.animation = ''; 
+	move_to(new_pos);
+	update_descriptor(new_text);
+}
+// Bring it as an onclick function
+window.selector = selector;
 
 animate();
